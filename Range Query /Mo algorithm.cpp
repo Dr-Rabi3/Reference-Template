6d60bade274010afe53ce7866bb7a7764c46d3bd
Mohamed Abdalrazek <91884590+Dr-Rabi3@users.noreply.github.com>
2023-12-07 13:46:16 +0200

    Update Mo algorithm.cpp

--- Range Query /Mo algorithm.cpp
+++ Range Query /Mo algorithm.cpp
@@ -1,29 +1,41 @@
-struct Query {
-  int l, r, id;
+template < typename T > class MO { // 1-based
+    struct Query { int l, r, id; };
+    const int M = 450;
+    vector <Query> queries;
+    vector<T> nums, ans;
+    int n, q, curAns;
+    void add(int x) { }
+    void rem(int x) { }
+public:
+    MO(int _n, int _q) : n(_n), q(_q) {
+        nums = vector<T>(n + 1);
+        ans = vector<T>(n + 1);
+        queries = vector<Query>(q);
+    }
+    void getArr() {
+        for (int i = 1; i <= n;++i)
+            cin >> nums[i];
+    }
+    void getQuery() {
+        for (int i = 1; i <= q;++i) {
+            int l, r;cin >> l >> r;
+            queries[i] = { l,r,i };
+        }
+    }
+    vector<T> mo_process() {
+        sort(all(queries), [&](Query& a, Query& b) {
+            int A = a.l / M;
+            int B = b.l / M;
+            return make_pair(A, a.r) < make_pair(B, b.r);
+            });
+        int l = queries[0].l, r = l - 1;
+        for (auto& query : queries) {
+            while (l > query.l) add(nums[--l]);
+            while (r < query.r) add(nums[++r]);
+            while (l < query.l) rem(nums[l++]);
+            while (r > query.r) rem(nums[r--]);
+            ans[query.id] = curAns;
+        }
+        return ans;
+    }
 };
-int curAns = 0;
-void add(int x) {
-
-}
-void rem(int x) {
-
-}
-const int M = 450;
-int n, q;
-int nums[N], ans[N];
-vector <Query> queries;
-void Mo() {
-  sort(all(queries), [&](Query& a, Query& b) {
-    int A = a.l / M;
-    int B = b.l / M;
-    return make_pair(A, a.r) < make_pair(B, b.r);
-    });
-  int l = queries[0].l, r = l - 1;
-  for (auto& query : queries) {
-    while (l > query.l) add(nums[--l]);
-    while (r < query.r) add(nums[++r]);
-    while (l < query.l) rem(nums[l++]);
-    while (r > query.r) rem(nums[r--]);
-    ans[query.id] = curAns;
-  }
-}
